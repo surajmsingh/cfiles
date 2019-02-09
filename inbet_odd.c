ba0917413eb2ef6d71554c01751ffabc8796f537
surajmsingh <40637255+surajmsingh@users.noreply.github.com>
2019-02-09 17:07:13 +0530

    Create inbet_odd.c

---
+++ inbet_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int a,b,i;
+   scanf("%d %d",&a,&b);
+
+   for(i=a+1;i<b;i++)
+   {
+     if(i%2 !=0 )
+     { printf("%d ",i);
+     }
+
+   }
+}
+//find the number inbetween the given number
