3a29f4cf986b3f00dc306dbedb417c844f323207
surajmsingh <40637255+surajmsingh@users.noreply.github.com>
2019-02-09 16:25:33 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int i ;
+    scanf("%d",&i);
+   if(i < 0)
+    printf("Negative");
+   else if (i > 0)
+    printf("Positive");
+   else
+    printf("Zero");
+}
